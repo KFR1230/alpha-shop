071bee7f9ae7916b59ec8e5a92593561b810d16b
KFR1230 <su0916111448@gmail.com>
2023-05-23 10:55:11 +0800

    add defaultChecked

--- src/main/progress/step/step2.jsx
+++ src/main/progress/step/step2.jsx
@@ -6,7 +6,7 @@ export default function ProgressStep2(){
             <h3 className={styles.formTitle}>運送方式</h3>
             <div className={`col col-12`}>
               <label className={`${styles.radioGroup} col col-12`} data-price="0">
-                <input id="shipping-standard" type="radio" name="shipping" checked={true} onChange={(e)=>e.target.checked}/>
+                <input id="shipping-standard" type="radio" name="shipping" defaultChecked/>
                 <div className={styles.radioInfo}>
                   <div className="col col-12">
                     <div className={styles.text}>標準運送</div>
